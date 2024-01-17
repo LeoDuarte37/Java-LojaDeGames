3d2bc75ade0c9ff789028b5414b30d7242bc60a8
Leo <leonardo.r.s.duarte@gmail.com>
2024-01-17 14:28:54 -0300

    Construção das tabelas na model + relacionamento entre Produto e Categoria

--- lojagames/src/main/java/com/generation/lojagames/model/Categoria.java
+++ lojagames/src/main/java/com/generation/lojagames/model/Categoria.java
@@ -1,5 +1,71 @@
 package com.generation.lojagames.model;
 
-public class Categoria {
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "Atributo nome é de preenchimento obrigatório")
+	@Size(min = 2, max = 30, message = "Atributo nome recebe no minimo 2 e no máximo 30 caracteres")
+	private String nome;
+	
+	@NotBlank(message = "Atributo descricao é de preenchimento obrigatório")
+	@Size(max = 300, message = "Atributo descricao recebe no máximo 300 caracteres")
+	private String descricao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
+	
 }
